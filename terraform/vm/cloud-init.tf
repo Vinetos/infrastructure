5a0b4030f5a80208db88f58c1ee8a9a9e9a3388e
Vinetos <valentin@vinetos.fr>
2023-12-29 09:45:05 +0100

    feat: terraform: dell: Update cloud-init configuration

--- terraform/vm/cloud-init.tf
+++ terraform/vm/cloud-init.tf
@@ -1,19 +1,19 @@
-resource "proxmox_virtual_environment_file" "ubuntu_container_template" {
+resource "proxmox_virtual_environment_file" "ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "dell"
 
   source_file {
-    path      = "https://cloud-images.ubuntu.com/jammy/20231215/jammy-server-cloudimg-amd64-disk-kvm.img"
-    file_name = "jammy-server-cloudimg-amd64-disk-kvm.img"
-    checksum = "f621e692d25c3f3e4cfa3120dfaabd17466cd8f381f46f485d37ad04dfd4baca"
+    path      = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+    file_name = "jammy-server-cloudimg-amd64.img"
+    checksum  = "7d776e3a287d5833fd9b68eb78094709ad4cf1e536f4802399cbd7da526ffc1d"
   }
 }
 
-resource "proxmox_virtual_environment_file" "cloud_config" {
+resource "proxmox_virtual_environment_file" "ubuntu_cloud_config" {
   content_type = "snippets"
   datastore_id = "proxmox"
-  node_name = var.pm_node_name
+  node_name    = var.pm_node_name
 
   source_file {
     path = "vm/cloud-init.yml"
